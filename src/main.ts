b7864879e9cd37900ee69dc42e579db0149d231d
JJKung <jj10102546@gmail.com>
2025-03-23 23:54:48 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./config/router"; // Ensure this is the correct path
 import "./style.css";
+import 'slick-carousel/slick/slick.css'; // Should work if installed correctly
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const app = createApp(App);
 app.use(createPinia()); // Register Pinia
